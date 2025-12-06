fee11efad44e79f856e9d99802a7061ac67f5011
PrinceKhimani78 <prince@mutanttechnologies.com>
2025-12-06 17:36:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 cd /home/demo.rojgariindia.com/app
-
 echo ">>> Starting Demo Next.js on port 3020"
-exec /usr/bin/npm start -- -p 3020 -H 0.0.0.0
+npm run start -- -p 3020 -H 0.0.0.0
