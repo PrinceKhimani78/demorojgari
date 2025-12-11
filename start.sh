7b3774934b1e86e019f5bc826f3023c2f039a2ee
PrinceKhimani78 <prince@mutanttechnololiges.com>
2025-12-11 18:39:17 +0530

    ssl

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/demo.rojgariindia.com/app/release
+
 echo ">>> Starting Demo Next.js on port 3020"
-npm run start -- -p 3020 -H 0.0.0.0
+
+# Use node_modules next binary
+./node_modules/.bin/next start -p 3020 -H 0.0.0.0
