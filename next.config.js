111eabfd5feb713e3cafe91d749b2c9f71fd29c6
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-16 18:44:41 +0530

    aaa

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    unoptimized: true, // ⬅️ THIS is the fix
+    remotePatterns: [
+      { protocol: "https", hostname: "randomuser.me" },
+      { protocol: "https", hostname: "1000logos.net" },
+      { protocol: "https", hostname: "logos-world.net" },
+      { protocol: "https", hostname: "imgcdn.stablediffusionweb.com" },
+      { protocol: "https", hostname: "static.vecteezy.com" },
+      { protocol: "https", hostname: "cdn-icons-png.flaticon.com" },
+      { protocol: "https", hostname: "seeklogo.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+    ],
   },
 
   eslint: {
