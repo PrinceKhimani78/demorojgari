fb3d3b5b05c6e2eed8f68ef1d2ee4a5ff174ab26
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-16 23:24:29 +0530

    aaa

--- start.sh
+++ start.sh
@@ -1,18 +1,9 @@
+# Path: /path/to/your/repo/start.sh
+
 #!/bin/bash
 set -e
-
-# Export environment variables
 export NODE_ENV=production
 export PORT=3021
-
-# Next.js will look for the .next folder from the current working directory.
-# The `deploy.yml` uploads the release folder into /app,
-# so we change directory into the root of the uploaded release.
 cd /home/demo.rojgariindia.com/app/release
-
-# Running npm install here is redundant if it's run in the PM2 restart step,
-# but kept here for robustness in case it's run manually.
-npm install --omit=dev
-
-# Start the Next.js production server
+# REMOVED: npm install --omit=dev 
 exec /usr/bin/env node ./node_modules/.bin/next start -p $PORT
