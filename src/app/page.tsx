bf2a78503ce9027e79ba76991e4c1799588f84c2
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-20 20:10:40 +0530

    awa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Home from "../components/Home/Home";
 
 export const metadata = {
-  title: "Home | Rojgari-India",
+  title: "Home |Rojgari-India",
   description: "Welcome to the homepage!",
 };
 
